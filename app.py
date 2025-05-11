d64f10419487743048aedb72e04632a284bb3519
Abhignya1206 <101594264+Abhignya1206@users.noreply.github.com>
2025-05-12 00:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def predict():
     return jsonify({'result': result, 'confidence': float(prediction[0][0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides this
+    app.run(host='0.0.0.0', port=port)
+
